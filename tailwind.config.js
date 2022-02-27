d0f13c9a630628b7866c68326b89e439d840b6fe
falolaseyi <falolasheyie@gmail.com.com>
2022-02-27 15:15:06 +0100

    start

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'grotesk': ['Space Grotesk', 'sans-serif'],
+        'body': ['Space Grotesk', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
