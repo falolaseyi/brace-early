d9c9b27afe6367760e7cefb7f74fd9d24af54a09
falolaseyi <falolasheyie@gmail.com.com>
2022-02-28 10:12:22 +0100

    fixed fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,9 +39,18 @@ module.exports = {
         'white': '#ffffff',
         'black': '#000000',
       },
+      
       fontFamily: {
-        'omny': ['omny', 'sans-serif'],
+        'custom': ['omny', 'sans-serif']
       },
+
+      fontSize: {
+        sm: ['14px', '20px'],
+        base: ['16px', '24px'],
+        lg: ['20px', '28px'],
+        xl: ['24px', '32px'],
+      }
+
     },
   },
   plugins: [],
